08f8f64be2e5c163bae11190a9c7cfded2726f97
rubina40 <rubinakarki318@gmail.com>
2021-02-25 10:18:03 +0545

    area

---
+++ lab1/labweekqn1.py
@@ -0,0 +1,6 @@
+#write a program that takes three numbers and prints their sum.every number is given on a seperate line.
+num1=int(input("enter num1"))
+num2=int(input("enter num2"))
+num3=int(input("enter num3"))
+sum=num1+num2+num3
+print(f"te sum of given number is,{sum}")
