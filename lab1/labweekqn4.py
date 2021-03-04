ffb04a9c446e7c6e836d66380f01dfee1ee7d208
rubina40 <rubinakarki318@gmail.com>
2021-03-04 09:53:06 +0545

    question no.4

---
+++ lab1/labweekqn4.py
@@ -0,0 +1,13 @@
+# given the integer N- the number of minutes that is passed since midnight
+# how many hours and minutes are displayed on the 24h digital clock?
+# the progam should print toe numbers: the number of hours (between 0 and 23)
+# and the number of minutes (between 0 and 59).
+# for example, if N=150,then 150 minutes have passed since midnight-i.e. now is 2:30 am.So the program
+# should print 2:30.
+
+N = int(input("enter minutes passed since midnight:"))
+hours = (N//60)
+minutes = (N%60)
+print(f"The hours is {hours}")
+print(f"The minutes is {minutes}")
+print(f"Its {hours}:{minutes} now")
