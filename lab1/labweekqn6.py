933521d8ef91f93eb89d14698d8d8a75085b4edf
rubina40 <rubinakarki318@gmail.com>
2021-03-17 09:48:14 +0545

    inital commit

---
+++ lab1/labweekqn6.py
@@ -0,0 +1,9 @@
+# solve each of the following problems using python scripts.
+# make sure you use appropriate variable names and comments.
+# when there is a final answer have python print it to the screen.
+# a person's body mass index (BMI) is defined as:
+# BMI = mass in kg(height in )^2
+mass=float(input("enter mass of the person in kg:"))
+height=float(input("enter height of the person in m:"))
+BMI=(mass/(height+height))
+print(f"The BMI of the person is {BMI}")
---
+++ lab1/labweekqn7.py
@@ -0,0 +1,2 @@
+# you live 4 miles away from university. the bus drives at 25mph but spends 2 minutes
+# at each of the 10 stops on the way
