5c759b378ed08d05024e0c31cc9250189d103f30
rubina40 <rubinakarki318@gmail.com>
2021-03-18 09:17:18 +0545

    labweekqn8

---
+++ lab1/labweekqn8.py
@@ -0,0 +1,6 @@
+# write a python program which accepts the radius of circle from the user and compute the area.
+#(area=pi*r^2)
+radius=float(input("Enter the radius of circle:"))
+pi=22//7
+area=pi*radius**2
+print(f"The area og this circle is {area}")
---
+++ lab1/labweekqn9.py
@@ -0,0 +1,5 @@
+# Write a python program to find sum of first n positive integers.
+# sum=(n*(n+1)/2)
+list={1,2,3,4,5,6,7,8,9,10}
+total=sum(list)
+print(f"The sum of given positive integer is {total}")
